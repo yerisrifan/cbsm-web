58088192beb70a725a5537eee2d80f1db422a6f0
yeris <yeris@yeriss-iMac-Pro.local>
2024-08-01 13:25:37 +0700

    add responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,52 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { FaInstagram, FaFacebook, FaTiktok, FaTwitter } from "react-icons/fa";
+import { FiMenu } from "react-icons/fi";
+import { Button } from "./ui/button";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const [showMenu, setShowMenu] = React.useState(false);
   return (
     <header className="sticky top-0 w-full z-50">
-      <div className="flex px-16 items-center justify-between  py-4 text-sm bg-transparent text-white">
+      <div className="flex px-2 md:px-16 items-center justify-between  py-4 text-sm bg-transparent text-white">
         <div className="flex  gap-4 items-center font-semibold ">
-          <div>
-            <img src="/logo.png" alt="logo" className="w-8 h-8" />
-          </div>
+          <img src="/logo.png" alt="logo" className="w-8 h-8" />
           <div className="border-l-1  border-gray-400"></div>
-          <div>
-            <ul className="flex gap-4 uppercase">
-              <Link href="/">Home</Link>
-
-              <Link href="/about">About</Link>
-
-              <Link href="/contact">Contact</Link>
-            </ul>
+          <div className="hidden md:flex gap-4 uppercase">
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
           </div>
         </div>
-        <div className="flex gap-4">
+        <div className="hidden md:flex gap-4">
           <FaInstagram />
           <FaFacebook />
           <FaTiktok />
           <FaTwitter />
         </div>
+        <div className="text-white cursor-pointer">
+          <FiMenu onClick={() => setShowMenu(!showMenu)} />
+        </div>
+      </div>
+      <div
+        className={
+          showMenu
+            ? "flex flex-col gap-2 text-sm p-2 absolute text-white bg-stone-900/80 w-full text-right "
+            : "hidden" + ""
+        }
+      >
+        <Link onClick={() => setShowMenu(!setShowMenu)} href="/">
+          Home
+        </Link>
+        <Link onClick={() => setShowMenu(!setShowMenu)} href="/about">
+          About
+        </Link>
+        <Link onClick={() => setShowMenu(!setShowMenu)} href="/contact">
+          Contact
+        </Link>
       </div>
     </header>
   );
